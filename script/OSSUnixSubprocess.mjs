e4a1a4bb95ef92ca999dd43922cf0f4cc550df79
HironoriIchikawa <sweatygarlic@yahoo.co.jp>
2021-10-02 01:02:50 +0900

    add mjs scripts

---
+++ script/OSSUnixSubprocess.mjs
@@ -0,0 +1,53 @@
+import cprocess from 'child_process';
+import stream from 'memory-streams';
+
+export class OSSUnixSubprocess {
+  constructor() {
+    this._command = "";
+    this._app = "";
+    this._arguments = [];
+    this._stdoutStream = null;
+    this._stderrStream = null;
+    this._exitStatus = 0;
+    this.errorMessage = "";
+  }
+  shellCommand_(cmd) {
+    this._command = cmd;
+  }
+  command_(cmd) {
+    this._app = cmd;
+  }
+  arguments_(list) {
+    this._arguments = list;
+  }
+  redirectStdout() {
+  }
+  redirectStderr() {
+  }
+  runAndWait() {
+    var cmd = this._command || (this._app + ' ' + this._arguments.join(' '));
+    try {
+      var stdout = cprocess.execSync(cmd).toString();
+      this._stdoutStream = new stream.ReadableStream(stdout);
+      this._stdoutStream.setEncoding('utf8');
+    } catch (error) {
+      this._exitStatus = error.status;
+      this.errorMessage = error.message;
+      this._stderrStream = new stream.ReadableStream(error.stderr);
+      this._stderrStream.setEncoding('utf8');
+      this._stdoutStream = new stream.ReadableStream(error.stdout);
+      this._stdoutStream.setEncoding('utf8');
+    }
+  }
+  isSuccess() {
+    return this._exitStatus == 0;
+  }
+  stdoutStream() {
+    return this._stdoutStream;
+  }
+  stderrStream() {
+    return this._stderrStream;
+  }
+  closeAndCleanStreams() {
+  }
+}
--- script/Pharo.mjs
+++ script/Pharo.mjs
@@ -1,11 +1,11 @@
 
-export class Transcript {};
-
-Transcript.show_ = function(msg) {
-  console.log(msg);
-};
-
+//-----------------------------------------------------------------------------
 export class PharoObject {
+
+  error_(msg) {
+    throw msg;
+  }
+
   assert_(aBlock) {
     var value = false;
     if (typeof aBlock == 'function') {
@@ -16,23 +16,55 @@ export class PharoObject {
     if(value === undefined || value === null || value === false)
         throw `Assertion failed: ${aBlock} failed.`;
   }
-}
 
+}
+PharoObject.error_ = function(msg) {
+  throw msg;
+};
+
+//-----------------------------------------------------------------------------
+export class Transcript {};
+
+Transcript.show_ = function(msg) {
+  console.log(msg);
+};
+
+//-----------------------------------------------------------------------------
+export class OSPlatform {}
+OSPlatform.current = function() {
+  return OSPlatform;
+};
+OSPlatform.isMacOS = function() {
+  return 'darwin' == process.platform;
+};
+
+//-----------------------------------------------------------------------------
 export class TestCase extends PharoObject {
   setUp() {
   }
   tearDown() {
   }
   assert_equals_(left, right) {
-    if (left != right)
+    var lvalue = null;
+    var rvalue = null;
+    if (typeof left == 'function') {
+      lvalue = left();
+    } else {
+      lvalue = left;
+    }
+    if (typeof right == 'function') {
+      rvalue = right();
+    } else {
+      rvalue = right;
+    }
+    if (lvalue != rvalue)
       throw `Assertion failed: ${left} != ${right}.`;
   }
 
-  error_(msg) {
-    throw msg;
-  }
 }
 
+
+//-----------------------------------------------------------------------------
 import stream from 'memory-streams';
 
 String.cr = function() { return '\r'; };
@@ -46,3 +78,126 @@ String.streamContents_ = function(aBlock) {
   aBlock(s);
   return s.toString();
 };
+
+//-----------------------------------------------------------------------------
+import fs from 'fs';
+import path from 'path';
+export class FileLocator {
+  constructor(path) {
+    this._payload = path;
+  }
+  isDirectory() {
+    return fs.statSync(this._payload).isDirectory();
+  }
+  exists() {
+    return fs.existsSync(this._payload);
+  }
+  fullName() {
+    return this._payload;
+  }
+  basename() {
+    return path.basename(this._payload);
+  }
+  creationTime() {
+    return fs.statSync(this._payload).ctime;
+  }
+  accessTime() {
+    return fs.statSync(this._payload).atime;
+  }
+  changeTime() {
+    return fs.statSync(this._payload).mtime;
+  }
+  relativeTo_(relBase) {
+    return new FileLocator(path.relative(this._payload, relBase.fullName()));
+  }
+  contains_(p) {
+    var abs = path.resolve(this._payload, p.fullName());
+    return abs.startsWith(this._payload);
+  }
+  parent() {
+    return new FileLocator(path.dirname(this._payload));
+  }
+  ensureCreateDirectory() {
+    var dir = path.dirname(this._payload);
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, {recursive: true});
+    }
+  }
+  join(p) {
+    return new FileLocator(path.resolve(this._payload, p));
+  }
+  writeStreamDo_(aBlock) {
+    var io = new SWFile(this._payload);
+    try {
+      aBlock(io);
+    } finally {
+      io.destroy();
+    }
+  }
+
+  children() {
+    var self = this;
+    return fs.readdirSync(this._payload).flatMap(function(name) {
+      return self.join(name);
+    });
+  }
+}
+
+FileLocator.imageDirectory = function() {
+  return new FileLocator(process.cwd());
+}
+
+//-----------------------------------------------------------------------------
+Date.prototype.prettyPrint = function() {
+  return this.toLocaleString();
+};
+
+//-----------------------------------------------------------------------------
+export class SWFile {
+  constructor(path) {
+    this._path = path;
+  }
+
+  truncate() {
+  }
+
+  position_(p) {
+  }
+
+  write_(contents) {
+    fs.writeFileSync(this._path, contents);
+  }
+
+  destroy() {
+  }
+}
+
+//-----------------------------------------------------------------------------
+export class Dictionary {}
+
+// for Dummy
+export class SWGFortranCompilerAdapter {}
+
+
+//-----------------------------------------------------------------------------
+if (process.env.NODE_ENV == 'test') {
+
+  var fl = FileLocator.imageDirectory();
+  console.log(fl.fullName());
+  console.log(fl.isDirectory());
+  console.log(fl.exists());
+  console.log(fl.basename());
+  console.log(fl.creationTime());
+  console.log(fl.accessTime());
+  console.log(fl.changeTime());
+  console.log(fl.relativeTo_(new FileLocator('/bin')).fullName());
+  var fl2 = fl.join('hoge/fuga.txt');
+  console.log(fl2.fullName());
+  console.log(fl.contains_(fl2));
+  fl2.ensureCreateDirectory();
+  fl2.writeStreamDo_(function(io) {
+    console.log(io._path);
+    io.write_('Hello, World\n');
+  });
+  console.log(fl.children());
+}
